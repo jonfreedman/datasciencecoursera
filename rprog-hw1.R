a2d48a542314cd6c8dc5973ec0bc45e1764c8bfc
Jon <jon.freedman@zoho.com>
2015-04-19 09:27:12 +0100

    use na.rm

--- rprog-hw1.R
+++ rprog-hw1.R
@@ -9,7 +9,8 @@ ozone47 <- data[["Ozone"]][47]
 ## ozone47 <- data[47, "Ozone"]
 ozoneMissing <- nrow(subset(data, is.na(Ozone)))
 ##ozoneMissing <- length(data$Ozone[is.na(data$Ozone)])
-ozoneMean <- mean(data$Ozone[complete.cases(data$Ozone)])
+##ozoneMean <- mean(data$Ozone[complete.cases(data$Ozone)])
+ozoneMean <- mean(data$Ozone, na.rm = TRUE)
 solarRFilteredMean <- mean(subset(data, Ozone > 31 & Temp > 90)$Solar.R)
 tempJuneMean <- mean(subset(data, Month == 6)$Temp)
 maxMayOzone <- max(subset(data, Month == 5 & !is.na(Ozone))$Ozone)
